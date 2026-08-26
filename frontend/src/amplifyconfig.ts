8b38a371c15fab0b51f2abf1d9e9c54fff943ed4
Dhananjaya Chamod <dhananjayahbc.23@uom.lk>
2026-08-26 22:39:34 +0530

    chore(frontend): update amplify configuration

--- frontend/src/amplifyconfig.ts
+++ frontend/src/amplifyconfig.ts
@@ -1,10 +1,16 @@
 export const amplifyConfig = {
+  Auth: {
+    Cognito: {
+      userPoolId: 'us-east-1_ADL42rErB',
+      userPoolClientId: 'mkcvp3g9vlq2kmn3phlrvjjv8',
+    },
+  },
   API: {
     GraphQL: {
       endpoint: 'https://esvvzyh4vbagfc2fqub3uxhtnm.appsync-api.us-east-1.amazonaws.com/graphql',
       region: 'us-east-1',
-      defaultAuthMode: 'apiKey' as const,
+      defaultAuthMode: 'apiKey',
       apiKey: 'da2-yf3dc3an75bupm3stcyfh4t5ru',
     },
   },
-};
+}
